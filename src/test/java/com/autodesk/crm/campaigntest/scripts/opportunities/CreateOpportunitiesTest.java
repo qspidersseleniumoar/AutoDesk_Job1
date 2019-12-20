ee327851af7f46fdc483698e34ad232e26e0072f
Anju2786 <anju.ms1986@gmail.com>
2019-12-20 17:28:25 +0530

    Opportunity pom,excel and test

--- src/test/java/com/autodesk/crm/campaigntest/scripts/opportunities/CreateOpportunitiesTest.java
+++ src/test/java/com/autodesk/crm/campaigntest/scripts/opportunities/CreateOpportunitiesTest.java
@@ -1,5 +1,40 @@
 package com.autodesk.crm.campaigntest.scripts.opportunities;
 
-public class CreateOpportunitiesTest {
 
+import com.autodesk.crm.commonlib.BaseClass;
+import com.autodesk.crm.objectrepository.opportunities.OpportunitiesPage;
+import com.autodesk.crm.objectrepository.common.HomePage;
+import org.testng.annotations.Test;
+/**
+ * 
+ * @author Anju
+ *
+ */
+public class CreateOpportunitiesTest extends BaseClass {
+	@Test
+	public void CreateOpportunities() throws Throwable{
+		/*1. Go to Opportunities page*/
+		OpportunitiesPage opportunitiesPage = homePage.navigateToOpportunities();
+	
+		/*Click the plus button to create Opportunity and enter valid data and save*/
+		
+		opportunitiesPage.createUsingPlus();
+		
+		/* and verify the Opportunity name&CampaignSource  in Opportunity info Page*/
+		opportunitiesPage.validatingOpportunityAndCampaignSource();
+	}
+	
+	/* AddingSalesStageHistorysToCreatedOpportunity*/
+	@Test
+	public void AddingSalesStage() throws Throwable{
+		OpportunitiesPage opportunitiesPage = homePage.navigateToOpportunities();
+		opportunitiesPage.AddingSalesStageHistory();
+	}
+	
+	/*AddingSalesOrderToCreatedOpportunity*/
+	@Test
+	public void AddingSalesOrder() throws Throwable{
+		OpportunitiesPage opportunitiesPage = homePage.navigateToOpportunities();
+		opportunitiesPage.AddingSalesOrder();
+}
 }
