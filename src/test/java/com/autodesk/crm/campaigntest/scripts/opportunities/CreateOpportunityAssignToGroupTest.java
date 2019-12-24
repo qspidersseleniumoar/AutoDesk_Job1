d3b89e422d33ab8091ef8cbe1379a9cd672c3d0d
USER <USER@LENOVO-PC>
2019-12-24 12:38:28 +0530

    2 more testcases

---
+++ src/test/java/com/autodesk/crm/campaigntest/scripts/opportunities/CreateOpportunityAssignToGroupTest.java
@@ -0,0 +1,24 @@
+package com.autodesk.crm.campaigntest.scripts.opportunities;
+import org.testng.annotations.Test;
+
+import com.autodesk.crm.commonlib.BaseClass;
+
+import com.autodesk.crm.objectrepository.opportunities.OpportunitiesPage;
+public class CreateOpportunityAssignToGroupTest extends BaseClass {
+	/**
+	 * 
+	 * 
+	 * @author Tess
+	 * createOpportunityAssignToGroup
+	 * @throws Throwable
+	 */
+	@Test
+	public void createOpportunityAssignToGroup() throws Throwable {
+
+		OpportunitiesPage oppurtunitiesPage = homePage.navigateToOpportunities();
+		oppurtunitiesPage.createOpportunitWithGroup();
+		oppurtunitiesPage.verifyOpportunitwithGroup();
+		
+	}
+//		
+}
---
+++ src/test/java/com/autodesk/crm/campaigntest/scripts/opportunities/CreateOpportunityWithCampaignSourceTest.java
@@ -0,0 +1,16 @@
+package com.autodesk.crm.campaigntest.scripts.opportunities;
+import org.testng.annotations.Test;
+import com.autodesk.crm.commonlib.BaseClass;
+import com.autodesk.crm.objectrepository.opportunities.OpportunitiesPage;
+/**
+ * @author Tess
+ *CreateOpportunityWithCampaignSource
+ */
+public class CreateOpportunityWithCampaignSourceTest extends BaseClass {
+	@Test
+	public void createOpportunityAssignToGroupTest() throws Throwable {
+		 OpportunitiesPage oppurtunitiesPage =homePage.navigateToOpportunities();
+		 oppurtunitiesPage.createOpportunityWithCampaign();           
+
+ }
+}
